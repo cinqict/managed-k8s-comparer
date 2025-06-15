2b029286de0e695e0493773af449e82c24dc03a0
Noud Savenije <noud.savenije@rabobank.nl>
2025-06-15 16:41:21 +0200

    feat: add k6 load testing setup to workflow and create load test script

---
+++ common/loadtest.js
@@ -0,0 +1,25 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+    vus: 10, // number of virtual users
+    duration: '30s', // total test duration
+};
+
+export default function () {
+    // Health check
+    let res = http.get('http://37.59.218.5/health');
+    check(res, { 'health is 200': (r) => r.status === 200 });
+
+    // Write endpoint
+    res = http.post('http://37.59.218.5/write', JSON.stringify({ content: 'Hello from k6!' }), {
+        headers: { 'Content-Type': 'application/json' },
+    });
+    check(res, { 'write is 200': (r) => r.status === 200 });
+
+    // Read endpoint
+    res = http.get('http://37.59.218.5/read');
+    check(res, { 'read is 200': (r) => r.status === 200 });
+
+    sleep(1);
+}

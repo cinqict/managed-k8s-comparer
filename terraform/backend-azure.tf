7b23e03372abd542d198ec1aa52736c728852d58
Noud Savenije <noud.savenije@rabobank.nl>
2025-06-22 17:34:26 +0200

    refactor: consolidate Terraform backend configuration and remove unused files

--- terraform/backend-azure.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-noudsavenije-devops"
-    storage_account_name = "noudstfbackend"
-    container_name       = "azuretfstate"
-    key                  = "terraform.tfstate"
-  }
-}
--- terraform/modules/azure/main.tf
+++ terraform/modules/azure/main.tf
@@ -1,28 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name  = "rg-noudsavenije-devops"
-    storage_account_name = "noudstfbackend"
-    container_name       = "azuretfstate"
-    key                  = "terraform.tfstate"
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-provider "random" {}
-
 data "azurerm_resource_group" "main" {
   name = "rg-noudsavenije-devops"
 }

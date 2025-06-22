1cdb0694c5d72e5a06910cbb992838bd06ac4a3d
Noud Savenije <noud.savenije@rabobank.nl>
2025-06-22 21:06:58 +0200

    fix: remove docker bridge CIDR from AKS configuration

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -81,7 +81,6 @@ resource "azurerm_kubernetes_cluster" "main" {
     network_plugin     = "azure"
     service_cidr       = "10.1.0.0/16"
     dns_service_ip     = "10.1.0.10"
-    docker_bridge_cidr = "172.17.0.1/16"
   }
 
   identity {

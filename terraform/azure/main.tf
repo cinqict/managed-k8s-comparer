240a62a677b63f7bbfb3f727abd4f0d74f4a1de6
Noud Savenije <noud.savenije@rabobank.nl>
2025-06-23 11:08:23 +0200

    style: format code for better readability in main.tf and providers.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_subnet" "db" {
   delegation {
     name = var.db_delegation_name
     service_delegation {
-      name = var.db_service_delegation_name
+      name    = var.db_service_delegation_name
       actions = var.db_service_delegation_actions
     }
   }
@@ -49,17 +49,17 @@ resource "azurerm_private_dns_zone_virtual_network_link" "postgres" {
 }
 
 resource "azurerm_postgresql_flexible_server" "main" {
-  name                   = var.psql_server_name
-  resource_group_name    = data.azurerm_resource_group.main.name
-  location               = data.azurerm_resource_group.main.location
-  version                = var.psql_version
-  administrator_login    = var.psql_admin_login
-  administrator_password = random_password.psql.result
-  storage_mb             = var.psql_storage_mb
-  sku_name               = var.psql_sku_name
-  delegated_subnet_id    = azurerm_subnet.db.id
-  zone                   = var.psql_zone
-  private_dns_zone_id    = azurerm_private_dns_zone.postgres.id
+  name                          = var.psql_server_name
+  resource_group_name           = data.azurerm_resource_group.main.name
+  location                      = data.azurerm_resource_group.main.location
+  version                       = var.psql_version
+  administrator_login           = var.psql_admin_login
+  administrator_password        = random_password.psql.result
+  storage_mb                    = var.psql_storage_mb
+  sku_name                      = var.psql_sku_name
+  delegated_subnet_id           = azurerm_subnet.db.id
+  zone                          = var.psql_zone
+  private_dns_zone_id           = azurerm_private_dns_zone.postgres.id
   public_network_access_enabled = var.psql_public_network_access_enabled
 }
 
@@ -77,19 +77,19 @@ resource "azurerm_kubernetes_cluster" "main" {
   dns_prefix          = var.aks_dns_prefix
 
   default_node_pool {
-    name       = var.aks_node_pool_name
-    node_count = var.aks_node_count
-    vm_size    = var.aks_vm_size
-    vnet_subnet_id = azurerm_subnet.aks.id
-    min_count  = var.aks_min_count
-    max_count  = var.aks_max_count
+    name                 = var.aks_node_pool_name
+    node_count           = var.aks_node_count
+    vm_size              = var.aks_vm_size
+    vnet_subnet_id       = azurerm_subnet.aks.id
+    min_count            = var.aks_min_count
+    max_count            = var.aks_max_count
     auto_scaling_enabled = var.aks_auto_scaling_enabled
   }
 
   network_profile {
-    network_plugin     = var.aks_network_plugin
-    service_cidr       = var.aks_service_cidr
-    dns_service_ip     = var.aks_dns_service_ip
+    network_plugin = var.aks_network_plugin
+    service_cidr   = var.aks_service_cidr
+    dns_service_ip = var.aks_dns_service_ip
   }
 
   identity {
--- terraform/azure/providers.tf
+++ terraform/azure/providers.tf
@@ -9,7 +9,7 @@ terraform {
       version = "~> 3.0"
     }
   }
-  
+
   backend "azurerm" {}
 }
 

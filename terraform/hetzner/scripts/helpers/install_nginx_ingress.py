f994d24e7e512e4828ed019c7eb1c51e0e9caef7
Noud Savenije <noud.savenije@rabobank.nl>
2025-08-09 17:47:01 +0200

    syntax

--- terraform/hetzner/scripts/helpers/install_nginx_ingress.py
+++ terraform/hetzner/scripts/helpers/install_nginx_ingress.py
@@ -5,11 +5,13 @@ import subprocess
 def now_utc():
     return datetime.now(timezone.utc).isoformat()
 
-def apply_template(path, namespace="default"):
+def apply_template(path, namespace=""):
     try:
-        subprocess.run([
-            "kubectl", "--kubeconfig", "kubeconfig.yaml", "-n", namespace, "apply", "-f", path], 
-            check=True)
+        cmd = ["kubectl", "--kubeconfig", "kubeconfig.yaml", "apply", "-f", path]
+        if namespace:
+            cmd.insert(3, "-n")
+            cmd.insert(4, namespace)
+        subprocess.run(cmd, check=True)
         return None
     except subprocess.CalledProcessError:
         return None
@@ -26,15 +28,15 @@ def create_hcloud_api_secret(hcloud_token, namespace):
     ], check=True)
 
 def main(hcloud_token):
-    print("Installing nginx ingress controller...")
-    apply_template("terraform/hetzner/scripts/templates/nginx.yaml", namespace="default")
-    print("Nginx ingress controller installed successfully.")
+    print("##### Installing nginx ingress controller... ######")
+    apply_template("terraform/hetzner/scripts/templates/nginx.yaml")
+    print("##### Nginx ingress controller installed successfully. #####")
 
-    print("Applying CCM networks configuration...")
+    print("##### Applying CCM networks configuration... #####")
     apply_template("terraform/hetzner/scripts/templates/ccm-networks.yaml", namespace="kube-system")
-    print("CCM networks configuration applied successfully.")
+    print("##### CCM networks configuration applied successfully. #####")
 
-    print("Create secret for Hetzner Cloud API token...")
+    print("##### Create secret for Hetzner Cloud API token... #####")
     create_hcloud_api_secret(hcloud_token=hcloud_token, namespace="kube-system")
-    print("Hetzner Cloud API token created successfully.")
+    print("##### Hetzner Cloud API token created successfully. #####")
 

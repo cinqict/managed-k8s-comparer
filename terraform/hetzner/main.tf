d7309522f3fa772a6fe064bf166b38e4a75f3629
Noud Savenije <noud.savenije@rabobank.nl>
2025-08-24 11:21:30 +0200

    refactor: remove commented-out worker node configuration from main.tf

--- terraform/hetzner/main.tf
+++ terraform/hetzner/main.tf
@@ -38,8 +38,6 @@ resource "hcloud_server" "master_node" {
   }
   network {
     network_id = hcloud_network.private_network.id
-    # IP Used by the master node, needs to be static
-    # Here the worker nodes will use 10.0.1.1 to communicate with the master node
     ip = "10.0.1.1"
   }
 
@@ -48,36 +46,6 @@ resource "hcloud_server" "master_node" {
 
   depends_on = [hcloud_network_subnet.private_network_subnet]
 }
-
-# data "template_file" "worker_cloud_init" {
-#   template = file("${path.module}/scripts/cloud-init-worker.yaml")
-#   vars = {
-#     worker_public_key  = file("${path.module}/worker_key.pub")
-#     worker_private_key = file("${path.module}/worker_key")
-#   }
-# }
-
-# resource "hcloud_server" "worker-nodes" {
-#   count = 1
-
-#   # The name will be worker-node-0, worker-node-1, worker-node-2...
-#   name        = "worker-node-${count.index}"
-#   image       = "ubuntu-24.04"
-#   server_type = "cax11"
-#   location    = "fsn1"
-#   public_net {
-#     ipv4_enabled = true
-#     ipv6_enabled = false
-#   }
-#   network {
-#     network_id = hcloud_network.private_network.id
-#   }
-#   user_data = data.template_file.worker_cloud_init.rendered
-
-#   ssh_keys = [hcloud_ssh_key.worker.id]
-
-#   depends_on = [hcloud_network_subnet.private_network_subnet, hcloud_server.master_node]
-# }
 
 resource "random_password" "postgresql" {
   length  = 16
